aa52775f1c2ac648b1b56573a105c5977c1a5dd8
Nikolai Nanev <niki4etooo@gmail.com>
2021-04-13 04:50:45 +0300

    added three buttons(add, edit, remove)

--- user_input.py
+++ user_input.py
@@ -10,6 +10,17 @@ class UserInput():
 		entry_label_node_id = Label(text="ID:")
 		entry_node_id = Entry(bg="black", fg="white")
 		
-		//To do
+		btn_add = Button(text="Add")
+		btn_edit = Button(text="Edit")
+		btn_remove = Button(text="Remove")
+		
+		#To do
 		entry_label_node_id.pack(side=LEFT)
 		entry_node_id.pack(side=LEFT)
+		btn_remove.pack(side=RIGHT)
+		btn_edit.pack(side=RIGHT)
+		btn_add.pack(side=RIGHT)
+		
+		
+		
+		
