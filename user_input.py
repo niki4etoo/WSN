5679143c3c834a6e8cea2504c76f235557da4b7b
Nikolai Nanev <niki4etooo@gmail.com>
2021-04-16 20:25:51 +0300

    added two more entries for name and position for node

--- user_input.py
+++ user_input.py
@@ -10,7 +10,10 @@ class UserInput():
 		#Entries
 		entry_label_node_id = Label(text="ID:")
 		entry_node_id = Entry(bg="black", fg="white")
-		
+		entry_label_node_name = Label(text="Name:")
+		entry_node_name = Entry(bg="black", fg="white")
+		entry_label_node_position = Label(text="Position:")
+		entry_node_position = Entry(bg="black", fg="white")
 		#Buttons
 		
 		btn_add = Button(text="Add")
@@ -23,6 +26,11 @@ class UserInput():
 		btn_remove.pack(side=RIGHT)
 		btn_edit.pack(side=RIGHT)
 		btn_add.pack(side=RIGHT)
+		
+		entry_label_node_name.pack(side=LEFT)
+		entry_node_name.pack(side=LEFT)
+		entry_label_node_position.pack(side=LEFT)
+		entry_node_position.pack(side=LEFT)
 		
 		
 		
