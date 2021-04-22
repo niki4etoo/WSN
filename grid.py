5fabe5a1235438310cd6935950d81c98a6c6718f
Nikolai Nanev <niki4etooo@gmail.com>
2021-04-22 11:12:52 +0300

    added properties to the grid and node class

--- grid.py
+++ grid.py
@@ -1,3 +1,9 @@
 class Grid:
-	def __init(self, name):
+	def __init__(self, name, width, height):
 		self.name = name
+		self.width = width
+		self.height = height
+		pass
+		
+	def description(self):
+		pass	#To Do
--- node.py
+++ node.py
@@ -1,6 +1,13 @@
 class Node:
-	def __init__(self, name, grid, posX, posY):
-		self.name = name 	# Name of the node
-		self.grid = grid	# Which grid the node will be placed
-		self.posX = posX	# Position X on the grid
-		self.posY = posY 	# Position Y on the grid
+	def __init__(self, name, grid, posX, posY, active):
+		self.name = name 	 # Name of the node
+		self.grid = grid	 # Which grid the node will be placed
+		self.posX = posX	 # Position X on the grid
+		self.posY = posY 	 # Position Y on the grid
+		self.active = active # Is active
+
+	def description(self):
+		pass
+		
+	
+		
