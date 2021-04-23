d5008f743d13e4e55ca710214aeae0210ed9834b
Nikolai Nanev <niki4etooo@gmail.com>
2021-04-23 08:13:14 +0300

    added path class for nodes

---
+++ path.py
@@ -0,0 +1,16 @@
+class Path:
+	active = False
+	
+	def __init__(self, name, length, firstNode, secondNode):
+		self.name = name
+		self.length = length
+		self.firstNode = firstNode
+		self.secondNode = secondNode
+	pass
+	
+	def is_active(self):
+		return active
+	pass
+	
+	
+	
