d1e1df6bcbd354a3afec5cd73fe319d1b71e50c4
Nikolai Nanev <niki4etooo@gmail.com>
2021-05-06 05:21:41 +0300

    added settings class

---
+++ settings.py
@@ -0,0 +1,16 @@
+class Settings:
+    def __init__(self):
+        #to do
+        
+    def window(self):
+        #to do
+        
+    def grid(self):
+        #to do
+        
+    def path(self):
+        #to do
+        
+    def node(self):
+        #to do
+        
