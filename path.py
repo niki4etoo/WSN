24611fd35bc8fff01b615ae7d731ac1ec1e50623
Nikolai Nanev <niki4etooo@gmail.com>
2021-06-13 22:26:00 +0300

    added path weight variable

--- path.py
+++ path.py
@@ -7,13 +7,15 @@ plt.show()
 
 class Path:
 	active = False
+	weight = [1, 10] #weight of the path
 	
-	def __init__():
+	def __init__(self, weight, active):
+		print(weight)
 		pass
 	
 	def is_active(self):
 	    return active
 	pass
 	
-	
+test_path = Path([1, 20], True)
 	
