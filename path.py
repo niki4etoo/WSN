66a092e1444b10f48d51e48fd003acd1a03d3dde
Nikolai Nanev <niki4etooo@gmail.com>
2021-06-14 21:27:12 +0300

    added length property of path class

--- path.py
+++ path.py
@@ -8,14 +8,16 @@ plt.show()
 class Path:
 	active = False
 	weight = [1, 10] #weight of the path
+	length = [0, 10]
 	
-	def __init__(self, weight, active):
+	def __init__(self, weight, length, active):
 		print(weight)
+		print(length)
 		pass
 	
 	def is_active(self):
 	    return active
 	pass
 	
-test_path = Path([1, 20], True)
+test_path = Path([1, 20], 50, True)
 	
