974155b271576b1e25c00b0e1594ff075cb45c58
Nikolai Nanev <niki4etooo@gmail.com>
2021-06-20 21:34:22 +0300

    added grid method

--- grid.py
+++ grid.py
@@ -1,9 +1,16 @@
 class Grid:
+	nodesCount = 0
+	
 	def __init__(self, name, width, height):
 		self.name = name
 		self.width = width
 		self.height = height
 		pass
+		
+	def grid(self, width, height, nodesCount):
+		self.width = width
+		self.height = height
+		self.nodesCount = nodesCount
 		
 	def description(self):
 		pass	#To Do
