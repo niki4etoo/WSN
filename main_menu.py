312efa486f1866a25fe2d0b608828afb89c690c1
Nikolai Nanev <niki4etooo@gmail.com>
2021-06-27 19:43:40 +0300

    added list for nodes

--- main_menu.py
+++ main_menu.py
@@ -3,16 +3,105 @@ from tkinter import *
 from tkinter.messagebox import showinfo
 
 
+#Each node parameters
+global node_name
+global node_weight
+global node_pos_x
+global node_pos_y
+
+global nodes
+nodes = []
+
+def add_nodes(i, nodes_count, name, weight, pos_x, pos_y, node_state):
+	node_name = name
+	node_weight = weight
+	node_pos_x = pos_x
+	node_pos_y = pos_y
+	
+	nodes.append(node_name)
+	nodes.append(node_weight)
+	nodes.append(node_pos_x)
+	nodes.append(node_pos_y)
+	nodes.append(node_state)
+	
+	nodes_frame = LabelFrame(text="Възел " + str(i+1))
+	node_name_entry_label = Label(nodes_frame, text="Име: ")
+	node_name_entry = Entry(nodes_frame, bg="white", fg="black", border=3)
+	node_weight_entry_label = Label(nodes_frame, text="Тежест: ")
+	node_weight_entry = Entry(nodes_frame, bg="white", fg="black", border=3)
+	node_pos_x_entry_label = Label(nodes_frame, text="Позиция по x: ")
+	node_pos_x_entry = Entry(nodes_frame, bg="white", fg="black", border=3)
+	node_pos_y_entry_label = Label(nodes_frame, text="Позиция по y: ")
+	node_pos_y_entry = Entry(nodes_frame, bg="white", fg="black", border=3)
+	node_state = IntVar()
+	node_start_radiobutton = Radiobutton(nodes_frame, text="Начален", variable=node_state, value="1")
+	node_end_radiobutton = Radiobutton(nodes_frame, text="Краен", variable=node_state, value="2")	
+	
+	
+	
+	if i == nodes_count:
+		node_button_add = Button(nodes_frame, text="Добави", state=DISABLED)
+		
+	else:
+		node_button_add = Button(nodes_frame, text="Добави", command=lambda: add_nodes(i+1, nodes_count, str(node_name_entry.get()), int(node_weight_entry.get()), int(node_pos_x_entry.get()), int(node_pos_y_entry.get()), node_state.get()))
+	
+	nodes_frame.grid(row=1, column=0, padx=20, pady=10)
+	node_name_entry_label.grid(row=1, column=0, sticky=E)
+	node_name_entry.grid(row=1, column=1, padx=10)
+	node_weight_entry_label.grid(row=2, column=0, sticky=E)
+	node_weight_entry.grid(row=2, column=1, padx=10)
+	node_pos_x_entry_label.grid(row=3, column=0, sticky=E)
+	node_pos_x_entry.grid(row=3, column=1, padx=10)
+	node_pos_y_entry_label.grid(row=4, column=0, sticky=E)
+	node_pos_y_entry.grid(row=4, column=1, padx=10)
+	
+	node_start_radiobutton.grid(row=5, column=0)
+	node_end_radiobutton.grid(row=5, column=1)
+	node_button_add.grid(row=6, column=1, columnspan=2)
+
 def newGridWithNodesCount(nodes_count):
+	# Frame for network parameters
+	network_parameters_frame = LabelFrame(text="Параметри", padx=10, pady=10)
 	if nodes_count >= 3:
 		global nodes_count_label
-		
-		# Frame for network parameters
-		network_parameters_frame = LabelFrame(text="Параметри", padx=10, pady=10)
-		
+		network_parameters_frame.grid(row=1, column=1, sticky=E, padx=10, pady=10)
 		nodes_count_label = Label(network_parameters_frame, text="Брой Възли: " + str(nodes_count))
-		nodes_count_label.grid(row=1, column=0)
-		network_parameters_frame.grid(row=2, column=0, sticky=W+E)
+		nodes_count_label.grid(row=0, column=2)
+		# Clear user input for nodes
+		button_add.grid_forget()
+		entry_node_id.grid_forget()
+		entry_label_node_id.grid_forget()
+		
+		global node_state
+		node_state = IntVar()
+		i = 0
+		nodes_frame = LabelFrame(text="Възел " + str(i+1))
+
+		node_name_entry_label = Label(nodes_frame, text="Име: ")
+		node_name_entry = Entry(nodes_frame, bg="white", fg="black", border=3)
+		node_weight_entry_label = Label(nodes_frame, text="Тежест: ")
+		node_weight_entry = Entry(nodes_frame, bg="white", fg="black", border=3)
+		node_pos_x_entry_label = Label(nodes_frame, text="Позиция по x: ")
+		node_pos_x_entry = Entry(nodes_frame, bg="white", fg="black", border=3)
+		node_pos_y_entry_label = Label(nodes_frame, text="Позиция по y: ")
+		node_pos_y_entry = Entry(nodes_frame, bg="white", fg="black", border=3)
+		node_start_radiobutton = Radiobutton(nodes_frame, text="Начален", variable=node_state, value="1")
+		node_end_radiobutton = Radiobutton(nodes_frame, text="Краен", variable=node_state, value="2")
+		node_button_add = Button(nodes_frame, text="Добави", command=lambda: add_nodes(i+1, nodes_count, str(node_name_entry.get()), int(node_weight_entry.get()), int(node_pos_x_entry.get()), int(node_pos_y_entry.get()), node_state.get()))
+		
+		nodes_frame.grid(row=1, column=0, padx=30, pady=10)
+		node_name_entry_label.grid(row=1, column=0, sticky=E)
+		node_name_entry.grid(row=1, column=1, padx=10)
+		node_weight_entry_label.grid(row=2, column=0, sticky=E)
+		node_weight_entry.grid(row=2, column=1, padx=10)
+		node_pos_x_entry_label.grid(row=3, column=0, sticky=E)
+		node_pos_x_entry.grid(row=3, column=1, padx=10)
+		node_pos_y_entry_label.grid(row=4, column=0, sticky=E)
+		node_pos_y_entry.grid(row=4, column=1, padx=10)
+		node_start_radiobutton.grid(row=5, column=0)
+		node_end_radiobutton.grid(row=5, column=1)
+		node_button_add.grid(row=6, column=1, columnspan=2)
+		
 	elif nodes_count < 2:
 		global error_message_low_nodes
 		error_message_low_nodes = Label(network_parameters_frame, text="Броят възли е по-малък от 2. Моля добавете повече от два възела")
@@ -20,6 +109,10 @@ def newGridWithNodesCount(nodes_count):
 		network_parameters_frame.grid(row=2, column=0, sticky=W+E)
 
 def createNewGrid():
+	global button_add
+	global entry_label_node_id
+	global entry_node_id
+	
 	entry_label_node_id = Label(text="Брой възли:")
 	entry_node_id = Entry(bg="white", fg="black", border=3)
 	
