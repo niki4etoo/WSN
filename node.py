89b320913887e3554bd80d57a1b6883d28a4b087
Nikolai Nanev <niki4etooo@gmail.com>
2021-07-22 22:45:35 +0300

    added print notes for each of the methods(add, remove, update)

--- node.py
+++ node.py
@@ -6,7 +6,8 @@ class Node:
 	weight = [1, 20]
 	active = True
 	edge_length = 0
-	
+	newNode = []
+	node = []
 	def __init__(self, name, grid, posX, posY, active):
 		self.name = name 	 # Name of the node
 		self.grid = grid	 # Which grid the node will be placed
@@ -17,12 +18,18 @@ class Node:
 		
 	def add(self, newNode):
 		#To DO
+		self.newNode = newNode
+		print("Added new node")
 		pass
 	
 	def remove(self, node):
 		#To DO
+		self.node = []
+		print("The node has been removed")		
 		pass
 	
 	def update(self, node):
 		#To DO
+		self.node = node
+		print("The node has been updated")
 		pass
