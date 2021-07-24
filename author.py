c96c8c2795eb2dea0cd75a947b23cc35b4cca846
Nikolai Nanev <niki4etooo@gmail.com>
2021-07-24 22:54:11 +0300

    added link to the github profile

--- author.py
+++ author.py
@@ -1,9 +1,22 @@
 from tkinter import messagebox
+import webbrowser
+
+
+
 
 class Author:
 	def __init__(self, fname, lname):
 		self.fname = fname
 		self.lname = lname
-		messagebox.showinfo(message="Относно автора: Името ми е Николай Нанев.", title="Относно")
-		print("Авторът на приложението е " + str(self.fname) + " " + str(self.lname))
+		result_answer = messagebox.askyesno(message="Относно автора: Името ми е Николай Нанев.", title="Относно")
+		if result_answer == 1:
+			self.opengithub()
+		else:
+			print("Авторът на приложението е " + str(self.fname) + " " + str(self.lname))
 	pass
+	
+	new = 1
+	url = "https://www.github.com/niki4etoo/"
+
+	def opengithub(self):
+		webbrowser.open(self.url,new=self.new)
