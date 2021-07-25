90789ce5e3b0a80fb8e8640b4aa01b150fdb4865
Nikolai Nanev <niki4etooo@gmail.com>
2021-07-25 23:13:40 +0300

    style changes

--- app.py
+++ app.py
@@ -9,9 +9,10 @@ class Application(tk.Frame):
 	def __init__(self, master=None):
 		super().__init__(master)
 		self.master = master
-		
 		menu = MainMenu.main_menu(self)
-	
+		
+		
+		
 	def exitProgram(self):
 		exit()
 
--- author.py
+++ author.py
@@ -1,10 +1,10 @@
 from tkinter import messagebox
 import webbrowser
 
-
-
-
 class Author:
+	new = 1
+	url = "https://www.github.com/niki4etoo/"
+	
 	def __init__(self, fname, lname):
 		self.fname = fname
 		self.lname = lname
@@ -14,9 +14,6 @@ class Author:
 		else:
 			print("Авторът на приложението е " + str(self.fname) + " " + str(self.lname))
 	pass
-	
-	new = 1
-	url = "https://www.github.com/niki4etoo/"
 
 	def opengithub(self):
 		webbrowser.open(self.url,new=self.new)
