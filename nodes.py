4c946fc4eed5c5e2a40d088a5921ba2b5d23360f
Nikolai Nanev <niki4etooo@gmail.com>
2021-07-27 22:28:49 +0300

    added three methods ( add, remove, update ) for each nodes list

--- nodes.py
+++ nodes.py
@@ -1,8 +1,21 @@
 from node import Node
 
 class Nodes(Node):
+	
 	def __init__(self, nodes):
 		super.__init__(self, name, grid, posX, posY, active)
 		self.nodes = nodes #To do
+		pass
+		
+	def add(self):
+		#To do
+		pass
+		
+	def remove(self, nodes):
+		#To Do
+		pass
+		
+	def update(self, nodes):
+		#To do
 		pass
 		
