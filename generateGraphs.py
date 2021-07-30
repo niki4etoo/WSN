95e20888bf1bb379396ef9e8313b8acb26f79670
Nikolai Nanev <niki4etooo@gmail.com>
2021-07-30 22:57:28 +0300

    removed old star graph

--- generateGraphs.py
+++ generateGraphs.py
@@ -23,7 +23,7 @@ starGraph.add_edges_from(starEdges)
 pos = nx.spring_layout(starGraph)
 plt.figure()
 nx.draw(starGraph, with_labels=True, font_weight='bold', node_size=800)
-plt.show()
+#plt.show()
 
 #To do
 starEdges = [['EP1', 'G'], ['EP2', 'G'], ['EP3', 'G'], ['EP4', 'G'], ['EP5', 'G']]
@@ -31,26 +31,4 @@ starGraph.add_edges_from(starEdges)
 pos = nx.spring_layout(starGraph)
 plt.figure()
 nx.draw(starGraph, with_labels=True, font_weight='bold', node_size=800)
-plt.show()
-
-testGraph = nx.Graph()
-testGraph.add_nodes_from([0, 10])
-testGraph.add_edge(1, 5)
-testGraph.add_edge(2, 5)
-testGraph.add_edge(3, 5)
-testGraph.add_edge(4, 5)
-testGraph.add_edge(6, 5)
-testGraph.add_edge(7, 5)
-testGraph.add_edge(8, 5)
-testGraph.add_edge(9, 5)
-testGraph.add_edge(0, 5)
-testGraph.add_edge(10, 5)
-weights = [ 1, 5, 3, 6, 8, 10, 13, 18, 20, 12 ]
-nx.set_edge_attributes(testGraph, values = weights, name = 'weights')
-testGraph.edges(data = True)
-nx.draw_networkx_edge_labels(testGraph, nx.spring_layout(testGraph),edge_labels={(1,5):1,\
-(2,5):5,(3,5):3},font_color='red')
-#Star graph
-nx.draw(testGraph, with_labels=True, font_weight='bold')
-plt.axis('off')
 plt.show()
