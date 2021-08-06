f27d081aa1dd67ad2efee5f3b9fda139a6d4aa15
Nikolai Nanev <niki4etooo@gmail.com>
2021-08-06 20:54:03 +0300

    added insert method and count, nodes_type properties

--- nodes.py
+++ nodes.py
@@ -2,12 +2,20 @@ from node import Node
 
 class Nodes(Node):
 	
+	count = 0
+	nodes_type = ""
+	
+	
 	def __init__(self, nodes):
 		super.__init__(self, name, grid, posX, posY, active)
 		self.nodes = nodes #To do
 		pass
 		
 	def add(self):
+		#To do
+		pass
+		
+	def insert(self, position):
 		#To do
 		pass
 		
