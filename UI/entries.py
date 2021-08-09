ed1b91e15a4c2095bc86f26c9c5fd6d828c1eb8f
Nikolai Nanev <niki4etooo@gmail.com>
2021-08-09 06:58:52 +0300

    ordering tree structure of folders and files

---
+++ UI/entries.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+class Entries:
+	
+	def __init__(self):
+	pass
+	
+	def node(self, entry_name):
+	pass
---
+++ input/user_input.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import *
+
+class UserInput():
+	def __init__(self, master=None):
+		self.master = master
+		self.user_input()
+
+	def user_input(self, master=None):
+		pass
