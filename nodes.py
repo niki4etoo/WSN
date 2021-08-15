9ea08eb3d9e38db768b0025999ff95c0d1ff9754
Nikolai Nanev <niki4etooo@gmail.com>
2021-08-15 20:58:42 +0300

    added nodes method for adding nodes

--- nodes.py
+++ nodes.py
@@ -4,15 +4,15 @@ class Nodes(Node):
 	
 	count = 0
 	nodes_type = ""
-	
+	nodes = []
 	
 	def __init__(self, nodes):
 		super.__init__(self, name, grid, posX, posY, active)
 		self.nodes = nodes #To do
 		pass
 		
-	def add(self):
-		#To do
+	def add(self, index, item):
+		self.nodes.insert(index, item)
 		pass
 		
 	def insert(self, position):
