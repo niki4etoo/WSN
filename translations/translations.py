a196854bc820f50aee782c36dd37c9b506ad06b3
Nikolai Nanev <niki4etooo@gmail.com>
2021-08-23 23:03:04 +0300

    added few words to translations

--- translations/translations.py
+++ translations/translations.py
@@ -1,7 +1,7 @@
 class Translations:
 	
-	bul_menu = [ "File": "Файл"]
-	eng_menu = [ ]
+	bul_menu = [ "File": "Файл", "Open": "Отвори", "Close": "Затвори" ]
+	eng_menu = [ "File": "File" ]
 	
 	def __init__(self):
 		pass
